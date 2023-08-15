33c47e1d752cb3e4119e4bef4d8e678762df5327
Jose Bohorquez <bd567358546@gmail.com>
2023-08-14 22:29:11 -0500

    ajustamos la pagina de inicio y faltan mas ajustes

--- Vistas/Paginas/inicio.php
+++ Vistas/Paginas/inicio.php
@@ -3,38 +3,12 @@
     <h4 id="inicio"></h4>
     <div class="row">
         <div class="col">
-            <h4 class="text-center">Nuestros Tecnicos Estan Altamente Calificados Y Experimentados En Estas Macas</h4>
+
         </div>
         <div class="col">
-
-
-<div id="carouselExampleIndicators" class="carousel slide">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="Vistas/Dependencias/Imgs/Marcas/abba.svg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="Vistas/Dependencias/Imgs/Marcas/challenger.svg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="Vistas/Dependencias/Imgs/Marcas/samsung.svg" class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-
+            <h4 class="text-center">Nuestros Tecnicos Estan Altamente Calificados Y Experimentados En Estas Macas:</h4>
+        </div>
+        <div class="col">
 
         </div>
     </div>
@@ -45,7 +19,7 @@
     <div class="row">
         <div class="col">
 
-            <div class="row row-cols-1 row-cols-md-5 g-4">
+            <div class="row row-cols-1 row-cols-sm-1 row-cols-md-5 g-4">
                 <div class="col">
                     <div class="card text-center" style="width: 18rem;">
                         <img src="Vistas/Dependencias/Imgs/nevecon/1.jpg" class="card-img-top" alt="...">
@@ -102,15 +76,29 @@
     </div>
     <br><br><br><br>
     <h4 id="contacto" class="text-center">Contacto</h4>
-    <div class="row">
+    <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2">
         <div class="col">
             <h4 class="text-center">Ubicacion</h4>
-            <div class="container">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3977.0302956471696!2d-74.15698202549791!3d4.588585942620301!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9f1df68f54a9%3A0x8dff723c79e03c87!2sCl.%2067a%20Sur%20%2365-26%2C%20Bogot%C3%A1!5e0!3m2!1ses-419!2sco!4v1691978264018!5m2!1ses-419!2sco" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <style>
+                .mapa {
+                    border: solid 1px;
+                    border-color: transparent;
+                    border-radius: 50px;
+                }
+
+                .mapaContent {
+                    border-radius: 50px;
+                }
+            </style>
+            <div class="container mapa">
+                <iframe class="mapaContent"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3977.0302956471696!2d-74.15698202549791!3d4.588585942620301!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9f1df68f54a9%3A0x8dff723c79e03c87!2sCl.%2067a%20Sur%20%2365-26%2C%20Bogot%C3%A1!5e0!3m2!1ses-419!2sco!4v1691978264018!5m2!1ses-419!2sco"
+                    width="100%" height="500px" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
         <div class="col">
-            
+
         </div>
     </div>
-</div>
+</div><br>
